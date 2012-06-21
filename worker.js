aebb7277a1d0596fc28b6837951b44b3799488d2
Francis Li <mail@francisli.com>
2012-06-21 15:14:41 -0700

    Re-written to pull a new pg connection off the pool for every message

--- worker.js
+++ worker.js
@@ -14,51 +14,55 @@ if (pgWebUrl == undefined) {
 	pgWebUrl = "tcp://localhost/web25c_development";
 }
 
-function processQueue(pgDataClient, pgWebClient, err, result) {
-	data = JSON.parse(result);
-	pgWebClient.query("SELECT id FROM users WHERE LOWER(uuid)=LOWER($1)", [ data.user_uuid ], function(err, result) {
-		if (err != null) {
-			console.log("could not query for user_uuid: " + err);
-		} else if (result.rows.length == 0) {
-			console.log("not found user_uuid=" + data.user_uuid);
-		} else if (result.rows.length == 1) {
-			var user_id = result.rows[0].id;
-			pgWebClient.query("SELECT id FROM buttons WHERE LOWER(uuid)=LOWER($1)", [ data.button_uuid ], function(err, result) {
-				if (err != null) {
-					console.log(err);
-				} else if (result.rows.length == 0) {
-					console.log("not found button_uuid=" + data.button_uuid);
-				} else if (result.rows.length == 1) {
-					var button_id = result.rows[0].id;
-					pgDataClient.query("INSERT INTO clicks (uuid, user_id, button_id, ip_address, user_agent, referrer, state, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [data.uuid, user_id, button_id, data.ip_address, data.user_agent, data.referrer, 0, data.created_at, new Date()], function(err, result) {
-						if (err != null) {
-							console.log(err);
-						} else {
-							console.log(data.created_at + ":" + user_id + ":" + button_id);
-						}
-					});
-				}
-			});
-		}
-	});
-	redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
-		processQueue(pgDataClient, pgWebClient, err, result);
-	})
-}
-
-pg.connect(pgDataUrl, function(err, pgDataClient) {
+function processQueue(err, result) {
 	if (err != null) {
-		console.log("Could not connect to data postgres: " + err);
+		console.log("redis brpoplpush error: " + err);
 	} else {
+		var data = JSON.parse(result);
 		pg.connect(pgWebUrl, function(err, pgWebClient) {
 			if (err != null) {
 				console.log("Could not connect to web postgres: " + err);
 			} else {
-				console.log("Starting queue processing...");
-				redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
-					processQueue(pgDataClient, pgWebClient, err, result);
-				})
+				pgWebClient.query("SELECT id FROM users WHERE LOWER(uuid)=LOWER($1)", [ data.user_uuid ], function(err, result) {
+					if (err != null) {
+						console.log("could not query for user_uuid: " + err);
+					} else if (result.rows.length == 0) {
+						console.log("not found user_uuid=" + data.user_uuid);
+					} else if (result.rows.length == 1) {
+						var user_id = result.rows[0].id;
+						pgWebClient.query("SELECT id FROM buttons WHERE LOWER(uuid)=LOWER($1)", [ data.button_uuid ], function(err, result) {
+							if (err != null) {
+								console.log(err);
+							} else if (result.rows.length == 0) {
+								console.log("not found button_uuid=" + data.button_uuid);
+							} else if (result.rows.length == 1) {
+								var button_id = result.rows[0].id;
+								pg.connect(pgDataUrl, function(err, pgDataClient) {
+									if (err != null) {
+										console.log("Could not connect to data postgres: " + err);
+									} else {
+										pgDataClient.query("INSERT INTO clicks (uuid, user_id, button_id, ip_address, user_agent, referrer, state, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [data.uuid, user_id, button_id, data.ip_address, data.user_agent, data.referrer, 0, data.created_at, new Date()], function(err, result) {
+											if (err != null) {
+												console.log(err);
+											} else {
+												console.log(data.created_at + ":" + user_id + ":" + button_id);
+											}
+										});
+									}
+								});
+							}
+						});
+					}
+				});
 			}
 		});
 	}
-});
+	redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
+		processQueue(err, result);
+	});
+}
+
+console.log("Starting queue processing...");
+redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
+	processQueue(err, result);
+})
