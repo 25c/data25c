241f210472cf2b2d5a79689342e3a8bf04484380
Francis Li <mail@francisli.com>
2012-07-05 17:17:19 -0700

    Pretty print formatting. Push into deduct queue after insert into db

--- worker.js
+++ worker.js
@@ -2,7 +2,8 @@ var pg = require('pg').native;
 var redis = require('redis-url');
 
 var QUEUE_KEY = 'QUEUE';
-var PROCESSING_KEY = 'QUEUE_PROCESSING';
+var QUEUE_PROCESSING_KEY = 'QUEUE_PROCESSING';
+var QUEUE_NEXT_KEY = 'QUEUE_DEDUCT';
 var redisDataClient = redis.connect(process.env.REDISTOGO_URL)
 
 var pgDataUrl = process.env.DATABASE_URL;
@@ -15,7 +16,7 @@ if (pgWebUrl == undefined) {
 }
 
 function processQueue(err, result) {
-    var remove = result; 
+	var remove = result; 
 	if (err != null) {
 		console.log("redis brpoplpush error: " + err);
 	} else {
@@ -29,11 +30,11 @@ function processQueue(err, result) {
 						console.log("could not query for user_uuid: " + err);
 					} else if (result.rows.length == 0) {
 						console.log("not found user_uuid = " + data.user_uuid);
-                        redisDataClient.lrem(PROCESSING_KEY, 0, remove, function(err, result) {
-                                                    if (err != null) {
-		                                                console.log("redis lrem error: " + err);
-                                                    }
-	                                            });
+						redisDataClient.lrem(QUEUE_PROCESSING_KEY, 0, remove, function(err, result) {
+							if (err != null) {
+								console.log("redis lrem error: " + err);
+							}
+						});
 					} else if (result.rows.length == 1) {
 						var user_id = result.rows[0].id;
 						pgWebClient.query("SELECT id FROM buttons WHERE LOWER(uuid) = LOWER($1)", [ data.button_uuid ], function(err, result) {
@@ -41,39 +42,39 @@ function processQueue(err, result) {
 								console.log(err);
 							} else if (result.rows.length == 0) {
 								console.log("not found button_uuid=" + data.button_uuid);
-                                redisDataClient.lrem(PROCESSING_KEY, 0, remove, function(err, result) {
-                                                    if (err != null) {
-		                                                console.log("redis lrem error: " + err);
-                                                    }
-	                                            });
+								redisDataClient.lrem(QUEUE_PROCESSING_KEY, 0, remove, function(err, result) {
+									if (err != null) {
+										console.log("redis lrem error: " + err);
+									}
+								});
 							} else if (result.rows.length == 1) {
 								var button_id = result.rows[0].id;
 								pg.connect(pgDataUrl, function(err, pgDataClient) {
 									if (err != null) {
 										console.log("Could not connect to data postgres: " + err);
 									} else {
-                                        pgDataClient.query("INSERT INTO clicks (uuid, user_id, button_id, ip_address, user_agent, referrer, state, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [data.uuid, user_id, button_id, data.ip_address, data.user_agent, data.referrer, 0, data.created_at, new Date()], function(err, result) {
+										pgDataClient.query("INSERT INTO clicks (uuid, user_id, button_id, ip_address, user_agent, referrer, state, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [data.uuid, user_id, button_id, data.ip_address, data.user_agent, data.referrer, 0, data.created_at, new Date()], function(err, result) {
 											if (err != null) {
-                                                if (err.routine = '_bt_check_unique') {
-												redisDataClient.lrem(PROCESSING_KEY, 0, remove, function(err, result) {
-                                                    if (err != null) {
-		                                                console.log("redis lrem error: " + err);
-                                                    }
-	                                            });
-                                                } else {
-                                                console.log("Not a uniqueness " + err);
-                                                }
+												if (err.routine = '_bt_check_unique') {
+													redisDataClient.lrem(QUEUE_PROCESSING_KEY, 0, remove, function(err, result) {
+														if (err != null) {
+															console.log("redis lrem error: " + err);
+														}
+													});
+												} else {
+													console.log("Not a uniqueness " + err);
+												}
 											} else {
 												console.log(data.created_at + ":" + user_id + ":" + button_id);
-                                                redisDataClient.lrem(PROCESSING_KEY, 0, remove, function(err, result) {
-                                                    if (err != null) {
-		                                                console.log("redis lrem error: " + err);
-                                                    }
-	                                            });
-                                            }
+												redisDataClient.multi().lrem(QUEUE_PROCESSING_KEY, 0, remove).lpush(QUEUE_NEXT_KEY, remove).exec(function(err, result) {
+													if (err != null) {
+														console.log("redis lrem/lpush error: " + err);
+													}	
+												});
+											}
 										});
-                                    }
-                                });
+									}
+								});
 							}
 						});
 					}
@@ -81,12 +82,12 @@ function processQueue(err, result) {
 			}
 		});
 	}
-    redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
-	    processQueue(err, result);
-    });
+	redisDataClient.brpoplpush(QUEUE_KEY, QUEUE_PROCESSING_KEY, 0, function(err, result) {
+		processQueue(err, result);
+	});
 }
 
 console.log("Starting queue processing...");
-redisDataClient.brpoplpush(QUEUE_KEY, PROCESSING_KEY, 0, function(err, result) {
+redisDataClient.brpoplpush(QUEUE_KEY, QUEUE_PROCESSING_KEY, 0, function(err, result) {
 	processQueue(err, result);
 });
