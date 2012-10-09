32a312f9cc7f7e14290e9be188c96e0648e17081
Francis Li <mail@francisli.com>
2012-10-09 15:09:41 -0700

    Modified migration to set a default value for funded_at

--- migrations/201210091437_add_funded_at_to_clicks.py
+++ migrations/201210091437_add_funded_at_to_clicks.py
@@ -1,4 +1,4 @@
 step(
-  "ALTER TABLE clicks ADD COLUMN funded_at timestamp without time zone;",
+  "ALTER TABLE clicks ADD COLUMN funded_at TIMESTAMP WITHOUT TIME ZONE; UPDATE clicks SET funded_at=updated_at WHERE state IN (2,3,4);",
   "ALTER TABLE clicks DROP COLUMN funded_at;"
 )
